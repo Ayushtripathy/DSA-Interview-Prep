17edd01d9541dbfb20732c3cb1ec3329d2009f3b
Ayush <tripathyayush123@gmail.com>
2022-06-22 19:10:53 +0530

    another-commit

--- Must-Do-Questions-GFG.cpp
+++ Must-Do-Questions-GFG.cpp
@@ -1,106 +1,70 @@
 //                               Arrays
 // Subarray with given sum
-// Count the triplets
 // Rearrange array alternatively
-// Number of pairs
-// Leaders in an array
-// Minimum Platforms
 // Reverse array in groups
 // Pythagorean Triplet
 // All Chocolate Distribution Problems
 // Element with left side smaller and right side greater
 // Convert array into Zig-Zag fashion
-// Last Index of 1
 // Spirally traversing a matrix
 // Largest Number formed from an Array
-// Maximum Index
-// Consecutive 1’s not allowed
-// Two numbers with sum closest to zero
-// Nuts and Bolts Problem
-// Boolean Matrix Problem
 // Smallest Positive missing number
-// Jumping Caterpillars
 // Array Product except itself
 // Cycle in a Circular Array
 // Ugly Numer 1
 // Count primes
-// Find Right Interval
 // Maximum distinct elements after removing k elements
 // Pascals Triangle
-// Insert Interval (medium): LC 57
-// Intervals Intersection (medium): LC 986
 // Find all Missing Numbers (easy): LC 448
 // Find the Duplicate Number (easy): LC 287
 // Find all Duplicate Numbers (easy): LC 442
-// Set Mismatch
 
 
 
 
 //                              Strings
 // Permutations of a given string
-// Recursively remove all adjacent duplicates
-// Check if string is rotated by two places
 // Remove Duplicates
 // Form a Palindrome
 // Longest Distinct Characters in the string
-// Most frequent word in an array of strings
 // CamelCase Pattern Matching
-// String Ignorance
 // Smallest window in a string containing all the characters of another string
 // Non Repeating Character
 // Check if strings are rotations of each other or not
-// Save Ironman
 // Repeated Character
 // Remove common characters and concatenate
-// Geek and its Colored Strings
 // Second most repeated string in a sequence
-// Wildcard matching
 // Regular Expression Matching
-// String valid shuffle
-// Find minimum operations required to transform a string into another string
+
 
 
 
 //                              Tree
 // Mirror a Tree
 // Construct tree from level order
-// Longest consecutive sequence in Binary tree
-// Check if all levels of two trees are anagrams or not.
 // Check if a binary tree is subtree of another binary tree				
 // Reconstruct itinerary
 // Print all root-to-leaf paths in any order
-// Delete Duplicate Folders in System
 
 
 
 //                       Dynamic Programming
 // Max length chain
-// Minimum number of jumps
 // Box Stacking
-// Path in Matrix
 // Minimum sum partition
-// Count number of ways to cover a distance
-// Optimal Strategy for a Game
 // Traveling Salesman
-// Paint fence (Permutation Combination)
 // Find optimal cost to construct a binary search tree
 // Unique BSTs	
 // Partition to K Equal Sum Subsets
 
 
 
-
-
 //                       Recursion & Backtracking
 // Generate IP Addresses
 // Word Boggle
-// Special Keyboard
 // Find shortest safe route in a path with landmines
 // Special Keyboard
 // Largest number in K swaps
-// String Permutations by changing case
-// Unique Generalized Abbreviations
 // Different Ways to Add Parentheses
 
 
